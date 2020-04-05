1de1581c64d8dd7a4c3d319cfaf901b1758464ff
chtinahow <chtinahoew@gmail.com>
2020-04-04 21:51:11 -0400

    Headers

--- src/components/HomepageHeader/HomepageHeader.js
+++ src/components/HomepageHeader/HomepageHeader.js
@@ -3,10 +3,22 @@ import { registerHtml } from 'tram-one'
 const html = registerHtml()
 
 export default (props, children) => {
-	return html`
-        <div>
-            <button class="button-primary button-purple">Create a Room</button>
-            <button class="button-primary button-round">Join a Room</button>
-        </div>
+  return html`
+    <div>
+      <header>
+        <h1 class="title">Space for your headline.</h1>
+        <h2 class="subtitle">A space for a more descriptive subtitle.</h2>
+        <p class="disclaimer">A small space for a disclaimer.</p>
+        <button class="get-started button button-primary button-large">Call to Action</button>
+
+        <a class="scroll-down" href="#"></a>
+      </header>
+
+      <section>  
+        <p class="h3"><strong>Get More</strong></p>
+        <p>This is an examples of header styles that are used in Mustard UI.  We like to think of it as a starter CSS framework that actually looks good.  We'd love to hear your feedback!</p>
+        <p>See more examples of <a href="https://mustard-ui.com/docs/header">CSS Header Styles</a>.</p>
+      </section>
+    </div>
   `
 }
--- src/components/LoginHeader/LoginHeader.js
+++ src/components/LoginHeader/LoginHeader.js
@@ -1,6 +1,7 @@
 import { registerHtml } from 'tram-one'
-import { signIn } from './components/GoogleAPI'
-import './LoginHeader.scss'
+// import { signIn } from './components/GoogleAPI'
+import './LoginHeader.scss';
+import BeerMug from '../../../clinking-beer-mugs_1f37b.png'
 
 const html = registerHtml()
 
@@ -9,6 +10,7 @@ export default (props, children) => {
     <nav class="LoginHeader">
 			<div class="nav-container">
 					<div class="nav-logo">
+							<img src=${BeerMug} width="50" height="50" />
 							<a href="/">BeercartIO</a>
 					</div>
 					<ul class="nav-links">
