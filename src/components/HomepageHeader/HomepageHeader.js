53af302831f7500a78ee8a990c95647619976e3a
chtinahow <chtinahoew@gmail.com>
2020-04-04 23:31:49 -0400

    remove some of the header code

--- src/components/HomepageHeader/HomepageHeader.js
+++ src/components/HomepageHeader/HomepageHeader.js
@@ -6,19 +6,11 @@ export default (props, children) => {
 	return html`
     <div>
       <header>
-        <h1 class="title">Space for your headline.</h1>
-        <h2 class="subtitle">A space for a more descriptive subtitle.</h2>
-        <p class="disclaimer">A small space for a disclaimer.</p>
-        <button class="get-started button button-primary button-large">Call to Action</button>
-
-        <a class="scroll-down" href="#"></a>
+        <h1 class="title">BeercartIO is a lobby for your virtual hangouts</h1>
+        <h2 class="subtitle">Works with Google Hangouts and Google Hangouts Meet</h2>
+        <button class="button button-primary">Create a Room</button>
+        <button class="button button-info">Join a Room</button>
       </header>
-
-      <section>
-        <p class="h3"><strong>Get More</strong></p>
-        <p>This is an examples of header styles that are used in Mustard UI.  We like to think of it as a starter CSS framework that actually looks good.  We'd love to hear your feedback!</p>
-        <p>See more examples of <a href="https://mustard-ui.com/docs/header">CSS Header Styles</a>.</p>
-      </section>
     </div>
   `
 }
