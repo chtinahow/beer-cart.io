1812572478626e30064667fb7361ce4d806200af
chtinahow <chtinahoew@gmail.com>
2020-04-04 20:37:44 -0400

    start of header code

--- src/components/ColorHeader/ColorHeader.js
+++ src/components/ColorHeader/ColorHeader.js
@@ -7,14 +7,16 @@ const html = registerHtml()
 export default () => {
 	const [color, incrementColor] = useColor()
 
-	return html`
-    <h1
-      class="color-header"
-      style="color:${color}"
-			role="heading"
-      onclick=${incrementColor}
-    >
-      beercart.io
-    </h1>
+  return html`
+    <div class="color-header">
+      <h1
+        style="color:${color}"
+        role="heading"
+        onclick=${incrementColor}
+      >
+        beercart.io
+      </h1>
+      <a style="color:${color}">Login</a>
+    </div>
   `
 }
