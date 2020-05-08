3cfc98c066ff884cc6cd6e97a2c97434d5560819
JRJurman <jrjurman@gmail.com>
2020-05-07 23:40:37 -0400

    better dark styles; sign in messaging

--- src/components/GoogleAuthDialog/GoogleAuthDialog.js
+++ src/components/GoogleAuthDialog/GoogleAuthDialog.js
@@ -23,7 +23,15 @@ export default (props, children) => {
         <div class="modal">
             <div class="modal-head">
                 <p class="modal-title">Please Sign In To Continue</p>
-            </div>
+						</div>
+						<div class="modal-body">
+							<h3>Sign in using Google Auth</h3>
+							<p>
+								Hello! We're going to ask you to sign in, and give us permissions for your calendar.
+								This is because we use the Calendar API to create the Hangout Conferencing used by beer-cart.io.
+								Don't worry though - no calendar information is read by our system.
+							</p>
+						</div>
             <div class="modal-footer">
               <button onclick=${onDismiss} class="button-info">Dismiss</button>
               <button onclick=${signIn} class="button-primary">Sign in</button>
